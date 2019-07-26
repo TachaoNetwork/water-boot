5d38f4e440c0163b32dfcebe940e2525ca41a467
duhao <dh@zjtachao.com>
2019-07-26 17:14:51 +0800

    日期工具类

--- water-boot-stater-common/src/main/java/com/zjtachao/fish/water/common/tool/WaterHttpUtil.java
+++ water-boot-stater-common/src/main/java/com/zjtachao/fish/water/common/tool/WaterHttpUtil.java
@@ -15,7 +15,11 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
@@ -24,11 +28,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 网络请求工具类
@@ -177,6 +184,84 @@ public class WaterHttpUtil {
 
             HttpContext context = new BasicHttpContext();
             response = httpClient.execute(request , context);
+
+            if((null != response) && (null != response.getStatusLine())
+                    && (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK)) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent(),StandardCharsets.UTF_8.name()));
+                String output = null;
+                while ((output = br.readLine()) != null){
+                    buffer.append(output);
+                }
+                result = buffer.toString();
+            }else {
+                logger.warn("响应错误，状态码："+response.getStatusLine().getStatusCode()+" URL："+url);
+            }
+        }catch (MalformedURLException ex){
+            logger.error("响应错误，URL："+url , ex);
+        }catch (IOException ex){
+            logger.error("IO错误，URL："+url , ex);
+        }catch (Exception ex){
+            logger.error("请求错误，URL："+url , ex);
+        }finally {
+            if(null != request){
+                request.releaseConnection();;
+            }
+            if(null != response){
+                try {
+                    response.close();
+                } catch (IOException ex) {
+                    logger.error("关闭链接错误",ex);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 提交Http请求 POST
+     * @param url 地址
+     * @param paramMap 普通参数
+     * @param fileMap 文件参数
+     * @return
+     */
+    public static String httpPostFile(String url , Map<String, String> paramMap, Map<String, String> fileMap) {
+        String result = null;
+        HttpPost request = null;
+        CloseableHttpResponse response = null;
+        try{
+            StringBuffer buffer = new StringBuffer();
+            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            request = new HttpPost(url);
+
+            HttpEntity reqEntity = null;
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+            //放置参数
+            if(null != paramMap && !paramMap.isEmpty()){
+                for(String key : paramMap.keySet()){
+                    StringBody body = new StringBody(paramMap.get(key), ContentType.TEXT_PLAIN);
+                    builder.addPart(key, body);
+                }
+            }
+            //放置文件参数
+            if(null != fileMap && !fileMap.isEmpty()){
+                for(String key : fileMap.keySet()){
+                    File file = new File(fileMap.get(key));
+                    FileBody fileBody = new FileBody(file);
+                    builder.addPart(key, fileBody);
+                }
+            }
+            reqEntity = builder.build();
+            request.setEntity(reqEntity);
+
+            //设置请求超时时间和传输超时时间
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setSocketTimeout(5000)
+                    .setConnectionRequestTimeout(5000)
+                    .build();
+            request.setConfig(requestConfig);
+
+            response = httpClient.execute(request);
 
             if((null != response) && (null != response.getStatusLine())
                     && (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK)) {
@@ -419,16 +504,11 @@ public class WaterHttpUtil {
     }
 
     public static void main(String [] args){
-        String getUrl = "http://127.0.0.1:8080/water/demo/test/redis/hello/123121";
-        System.out.print(httpGet(getUrl));
-        String postUrl = "http://127.0.0.1:8080/water/demo/test/annotations/save";
-        String content = "{\n" +
-                "\t\"userCode\":\"123456\",\n" +
-                "\t\"userNickName\":\"昵称\",\n" +
-                "\t\"userAge\":20,\n" +
-                "\t\"deleteFlag\":\"1\"\n" +
-                "}";
-        System.out.print(httpPost(postUrl , content));
+       String url = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=23_c6cKf3waBLq3y0g015gOHgTZ2QVzdpkM2KonrvVpHTYkjw4KRMvktngU5XoCELadY6D1Ayujl62QmCa6Cyy58M9LIbmQZRMUjNRX01UH30QTN0WiOLus80C66b4sk0qMI7RCrdAiwCoassysKJBfAAAZYQ&type=image";
+       Map<String , String> fileMap = new HashMap<String , String>();
+       fileMap.put("media", "/Users/duhao/tmp/eyou/img/product/2019/05/05/0134b6241a1f40ababe81d72b128b86b.jpg");
+       String result = httpPostFile(url, null, fileMap);
+       System.out.println(result);
     }
 
 }
